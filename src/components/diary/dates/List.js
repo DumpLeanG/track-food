eef535e1bb0fbb782cb14d17aa8bde356c6fdc25
Михаил <isip_m.d.chinenov@mpt.ru>
2025-04-10 17:33:09 +0300

    small fixes to date

--- src/components/diary/dates/List.js
+++ src/components/diary/dates/List.js
@@ -9,6 +9,7 @@ import { IsDeviceContext } from '@/lib/IsDeviceContext';
 export default function List({ day, setDay }) {
     const { isLaptop } = useContext(IsDeviceContext);
     const currentDates = getWeekDays(day);
+    
     return (
         <ul className={styles.diary_dates_list}>
             {currentDates.map((currentDate) => (
@@ -17,7 +18,10 @@ export default function List({ day, setDay }) {
                     handleClick={() => setDay(currentDate)}
                     date={currentDate}
                     dateText={isLaptop ? `${currentDate.getDate()}.${currentDate.getMonth() + 1}` : `${weekDays[currentDate.getDay()]} / ${currentDate.getDate()}.${currentDate.getMonth() + 1}`}
-                    isActive={currentDate.toDateString() === day.toDateString()}
+                    isActive={
+                        `${currentDate.getFullYear()}-${currentDate.getMonth() + 1 < 10 ? '0' : ''}${currentDate.getMonth() + 1}-${currentDate.getDate() < 10 ? '0' : ''}${currentDate.getDate()}`
+                        === `${day.getFullYear()}-${day.getMonth() + 1 < 10 ? '0' : ''}${day.getMonth() + 1}-${day.getDate() < 10 ? '0' : ''}${day.getDate()}`
+                    }
                 />
             ))}
         </ul>
